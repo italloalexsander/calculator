0cddcf3bdee40721871857c7b3d71bf78818284f
Itallo <italloalexsander@gmail.com>
2022-05-22 18:38:51 -0300

    Add eventlistener to each buttons

--- script.js
+++ script.js
@@ -14,3 +14,14 @@ const operate = (operator, firstNumber, secondNumber) => {
     }
 }
 
+
+//Use the spread operator to make the HTML collection into an Array so it can use
+//Array methods
+const digits = [...document.getElementsByClassName('digits')]
+
+digits.forEach(element => {
+    element.addEventListener('click', inputHandler());
+});
+
+
+    
